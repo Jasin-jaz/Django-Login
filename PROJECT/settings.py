b33eeaa4b0708457d9b5cc9b21b459cbcbcf341a
Jasin-jaz <Jasintp707@gmail.com>
2024-04-27 09:24:25 +0530

    auth_app new folder created

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'login',
     'rest_framework.authtoken',
-
+    'auth_app',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'auth_app.User'
